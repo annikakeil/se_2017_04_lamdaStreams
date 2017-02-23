55f30917be7fa54547c3eea3e91d000c008171f7
Annika Keil <annika.keil95@gmail.com>
2017-02-23 10:42:20 +0100

    Records mit Werten aus der hsqldb angelegt und in ArrayList gespeichert

--- sorting_with_two_fields/src/Application.java
+++ sorting_with_two_fields/src/Application.java
@@ -10,15 +10,16 @@ public class Application {
     }
 
     public void init() {
-        Record record01 = new Record("A",1);
-        Record record02 = new Record("C",2);
-        Record record03 = new Record("B",1);
-        Record record04 = new Record("B",2);
-        Record record05 = new Record("A",3);
-        Record record06 = new Record("C",2);
-        Record record07 = new Record("A",3);
-        Record record09 = new Record("A",1);
-        Record record10 = new Record("B",2);
+        Record record01 = new Record(1,"M1",1,"A",10,4.99,"A");
+        Record record02 = new Record(2,"M1",1,"A",12,5.99,"B");
+        Record record03 = new Record(3,"M1",2,"A",8,5.99,"B");
+        Record record04 = new Record(4,"M1",2,"A",10,8.99,"A");
+        Record record05 = new Record(5,"M2",2,"C",10,14.99,"C");
+        Record record06 = new Record(6,"M3",2,"B",6,8.99,"B");
+        Record record07 = new Record(7,"M2",2,"A",10,6.99,"A");
+        REcord record08 = new Record(8,"M1",3,"B",10,11.99,"C")
+        Record record09 = new Record(9,"M1",3,"B",5,8.99,"B");
+        Record record10 = new Record(10,"M2",4,"C",18,5.99,"C");
         records = Arrays.asList(record01,record02,record03,record04,record05,record06,record07,record09,record10);
     }
 
