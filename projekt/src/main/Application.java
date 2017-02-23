9f4f98953501faeb38cde78f7ca7d5acf2d72b53
JohannesLaier <JohannesLaier@gmail.com>
2017-02-23 10:43:05 +0100

    Daten ins Template verschoben.

--- projekt/src/main/Application.java
+++ projekt/src/main/Application.java
@@ -4,12 +4,29 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Application implements IApplication {
-    private ArrayList<Record> records;
+    private List<Record> records;
 
     public Application() {
         records = new ArrayList<>();
+		
+		Record record01 = new Record(1,"M1",1,'A',10,4.99,'A');
+        Record record02 = new Record(2,"M1",1,'A',12,5.99,'B');
+        Record record03 = new Record(3,"M1",2,'A',8,5.99,'B');
+        Record record04 = new Record(4,"M1",2,'A',10,8.99,'A');
+        Record record05 = new Record(5,"M2",2,'C',10,14.99,'C');
+        Record record06 = new Record(6,"M3",2,'B',6,8.99,'B');
+        Record record07 = new Record(7,"M2",2,'A',10,6.99,'A');
+        Record record08 = new Record(8,"M1",3,'B',10,11.99,'C');
+        Record record09 = new Record(9,"M1",3,'B',5,8.99,'B');
+        Record record10 = new Record(10,"M2",4,'C',18,5.99,'C');
+		
+        records = Arrays.asList(record01, record02, record03, record04, record05, record06, record07, record09, record10);
     }
 
     public long convertDateStringToUnixSeconds(String dateString) {
@@ -36,7 +53,22 @@ public class Application implements IApplication {
 
     public void executeQuery02() {
         System.out.println("--- query02");
-        System.out.println();
+        /*
+        double max = 0;
+        for (Record r : records) {
+            if (r.getCustomerType() == 'A'){
+                if (r.getPrice() > max){
+                    max = r.getPrice();
+                }
+            }
+        }
+        */
+        Predicate<Record> filterPredicate = record -> record.getCustomerType() == 'A';
+        Comparator<Record> comparator = (r1, r2) -> Double.compare(r1.getPrice(), r2.getPrice());
+
+        double max = records.stream().filter(filterPredicate).max(comparator).get().getPrice();
+
+        System.out.println("Maximum: " + max);
     }
 
     public void executeQuery03() {
--- projekt/src/main/Record.java
+++ projekt/src/main/Record.java
@@ -1,4 +1,90 @@
 package main;
 
 public class Record {
+
+    private int id;
+    private String movie;
+    private int week;
+    private char rowID;
+    private int seat;
+    private double price;
+    private char customerType;
+
+    public Record(int id, String movie, int week, char rowID, int seat, double price, char customerType) {
+        this.id = id;
+        this.movie = movie;
+        this.week = week;
+        this.rowID = rowID;
+        this.seat = seat;
+        this.price = price;
+        this.customerType = customerType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public char getRowID() {
+        return rowID;
+    }
+
+    public void setRowID(char rowID) {
+        this.rowID = rowID;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public char getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(char customerType) {
+        this.customerType = customerType;
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("ID: ").append(id).append(", ");
+        stringBuilder.append("Movie: ").append(movie).append(", ");
+        stringBuilder.append("Week: ").append(week).append(", ");
+        stringBuilder.append("Row: ").append(rowID).append(", ");
+        stringBuilder.append("Seat: ").append(seat).append(", ");
+        stringBuilder.append("Price: ").append(price).append(", ");
+        stringBuilder.append("Customer Type: ").append(customerType);
+        return stringBuilder.toString();
+    }
 }
